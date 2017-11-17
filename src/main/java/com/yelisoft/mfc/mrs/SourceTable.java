845c4c581fd5df9f82f8c8f9e8e35a6261c9a8f6
Yuriy Yeliseyev <yuriye@mail.ru>
2017-11-17 15:50:07 +1200

    After start

---
+++ src/main/java/com/yelisoft/mfc/mrs/SourceTable.java
@@ -0,0 +1,12 @@
+package com.yelisoft.mfc.mrs;
+
+public class SourceTable {
+    private String name;
+    private String nameSourceColumn;
+    private String fistSourceColumn;
+    private String secondSourceColumn;
+    private int startRow;
+
+
+
+}
