767ddd1e1f9de428c79bbf0a7081663f849cfcbf
Yuriy Yeliseyev <yuriye@mail.ru>
2017-11-20 17:19:03 +1200

    test 2

--- src/main/java/com/yelisoft/mfc/mrs/Test.java
+++ src/main/java/com/yelisoft/mfc/mrs/Test.java
@@ -13,45 +13,29 @@ public class Test {
         Yaml yaml = new Yaml();
         Config config = null;
 
-        if (args.length > 0) {
+        if (args.length > 0 && "1".equals(args[0])) {
             System.out.println("Arg[0]==" + args[0]);
-            if ("1".equals(args[0])) {
-                config = new Config();
-                config.setNamesComplianceFile("nameComplianceFileStub");
-                Map<String, String> pageNamesCompliance = new HashMap<>();
-                pageNamesCompliance.put("sPage1", "dPage1");
-                pageNamesCompliance.put("sPage2", "dPage2");
-                pageNamesCompliance.put("sPage3", "dPage3");
-                pageNamesCompliance.put("sPage4", "dPage4");
-                pageNamesCompliance.put("sPage5", "dPage5");
-                pageNamesCompliance.put("sPage6", "dPage6");
-                DestinationPlan dp1 = new DestinationPlan("authorityLevelInstance1",
-                        "destinationFileName1",
-                        pageNamesCompliance);
-                Map<String, DestinationPlan> destinasionsMap = new HashMap<>();
-                destinasionsMap.put("authorityLevelInstance1", dp1);
+            config = new Config();
+            config.setNamesComplianceFile("nameComplianceFileStub");
+            Map<String, String> pageNamesCompliance = new HashMap<>();
+            pageNamesCompliance.put("sPage1", "dPage1");
+            pageNamesCompliance.put("sPage2", "dPage2");
+            pageNamesCompliance.put("sPage3", "dPage3");
+            pageNamesCompliance.put("sPage4", "dPage4");
+            pageNamesCompliance.put("sPage5", "dPage5");
+            pageNamesCompliance.put("sPage6", "dPage6");
+            DestinationPlan dp1 = new DestinationPlan("authorityLevelInstance1",
+                    "destinationFileName1",
+                    pageNamesCompliance);
+            Map<String, DestinationPlan> destinasionsMap = new HashMap<>();
+            destinasionsMap.put("authorityLevelInstance1", dp1);
 
-                config.setAuthorityToDestinationPlan(destinasionsMap);
-            }
-            else {
-//                for (Object obj: yaml.loadAll(new FileReader("C:/config.yml"))
-//                     ) {
-//                    StringWriter sw = new StringWriter();
-//                    yaml.dump((Config) obj , sw);
-//                    System.out.println(sw.toString());
-//                    System.out.println(obj);
-//                }
-////                System.out.println(yaml.loadAll(new FileReader("C:/config.yml")));
-                config = yaml.load(new FileReader("C:/config.yml"));
-            }
-        }
-        else {
-//            System.out.println(yaml.loadAll(new FileReader("C:/config.yml")));
+            config.setAuthorityToDestinationPlan(destinasionsMap);
+        } else {
             config = yaml.load(new FileReader("C:/config.yml"));
         }
         StringWriter sw = new StringWriter();
         yaml.dump(config, sw);
         System.out.println(sw.toString());
-
     }
 }
